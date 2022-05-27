3f938d86194e9b6bf9fd21be474bdbeb698c804b
mattuser <matheus.stsousa@gmail.com>
2022-05-26 22:02:43 -0300

    adicionando teste

--- testcalc.py
+++ testcalc.py
@@ -7,19 +7,19 @@ class Testes(TestCase):
         tester = app.test_client(self)
         response = tester.post('/calc', data=dict(n1='10', n2='2', operation='potencia'))
         self.assertEqual(response.status, '200 OK')
-       # self.assertTrue(b'100.0' in response.data)
+        self.assertTrue(b'100.0' in response.data)
 
     def test_raiz_quadrada(self):
         tester = app.test_client(self)
         response = tester.post('/calc', data=dict(n1='81', operation='raiz_quadrada'))
         self.assertEqual(response.status, '200 OK')
-        #self.assertTrue(b'9.0' in response.data)
+        self.assertTrue(b'9.0' in response.data)
 
     def test_logaritmo(self):
         tester = app.test_client(self)
         response = tester.post('/calc', data=dict(n1='81', operation='logaritmo'))
         self.assertEqual(response.status, '200 OK')
-        #self.assertTrue(b'1.9084850188786497' in response.data)
+        self.assertTrue(b'1.9084850188786497' in response.data)
 
 if __name__ == '__main__':
     main()
